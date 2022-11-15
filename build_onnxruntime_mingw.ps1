b49cbe2b13cf99a0c4a73c35fe7fcfe1bb7d0a5e
yupeng <yupeng@fotoit.cn>
2022-11-15 17:00:55 +0800

    fix: fix command format

--- build_onnxruntime_mingw.ps1
+++ build_onnxruntime_mingw.ps1
@@ -10,31 +10,31 @@ function BuildForWindows($platform, $build_type) {
         $platform_flag = "-m64"
     }
 
-    cmake -G "MinGW Makefiles" \
-        -D CMAKE_C_FLAGS=$platform_flag \
-        -D CMAKE_CXX_FLAGS=$platform_flag \
-        -D CMAKE_BUILD_TYPE=${build_type} \
-        -D CMAKE_INSTALL_PREFIX=/mingw64 \
-        -D Protobuf_USE_STATIC_LIBS=ON \
-        -D eigen_SOURCE_PATH=/mingw64/include/eigen3 \
-        -D onnxruntime_BUILD_BENCHMARKS=OFF \
-        -D onnxruntime_BUILD_SHARED_LIB=ON \
-        -D onnxruntime_BUILD_UNIT_TESTS=OFF \
-        -D onnxruntime_ENABLE_PYTHON=OFF \
-        -D onnxruntime_ENABLE_STATIC_ANALYSIS=OFF \
-        -D onnxruntime_ENABLE_TRAINING=OFF \
-        -D onnxruntime_GENERATE_TEST_REPORTS=OFF \
-        -D onnxruntime_PREFER_SYSTEM_LIB=ON \
-        -D onnxruntime_RUN_ONNX_TESTS=OFF \
-        -D onnxruntime_USE_AVX2=ON \
-        -D onnxruntime_USE_AVX512=ON \
-        -D onnxruntime_USE_AVX=ON \
-        -D onnxruntime_USE_CUDA=OFF \
-        -D onnxruntime_USE_DNNL=OFF \
-        -D onnxruntime_USE_FULL_PROTOBUF=ON \
-        -D onnxruntime_USE_MPI=ON \
-        -D onnxruntime_USE_OPENMP=ON \
-        -D onnxruntime_USE_PREINSTALLED_EIGEN=ON \
+    cmake -G "MinGW Makefiles" `
+        -D CMAKE_C_FLAGS=$platform_flag `
+        -D CMAKE_CXX_FLAGS=$platform_flag `
+        -D CMAKE_BUILD_TYPE=${build_type} `
+        -D CMAKE_INSTALL_PREFIX=/mingw64 `
+        -D Protobuf_USE_STATIC_LIBS=ON `
+        -D eigen_SOURCE_PATH=/mingw64/include/eigen3 `
+        -D onnxruntime_BUILD_BENCHMARKS=OFF `
+        -D onnxruntime_BUILD_SHARED_LIB=ON `
+        -D onnxruntime_BUILD_UNIT_TESTS=OFF `
+        -D onnxruntime_ENABLE_PYTHON=OFF `
+        -D onnxruntime_ENABLE_STATIC_ANALYSIS=OFF `
+        -D onnxruntime_ENABLE_TRAINING=OFF `
+        -D onnxruntime_GENERATE_TEST_REPORTS=OFF `
+        -D onnxruntime_PREFER_SYSTEM_LIB=ON `
+        -D onnxruntime_RUN_ONNX_TESTS=OFF `
+        -D onnxruntime_USE_AVX2=ON `
+        -D onnxruntime_USE_AVX512=ON `
+        -D onnxruntime_USE_AVX=ON `
+        -D onnxruntime_USE_CUDA=OFF `
+        -D onnxruntime_USE_DNNL=OFF `
+        -D onnxruntime_USE_FULL_PROTOBUF=ON `
+        -D onnxruntime_USE_MPI=ON `
+        -D onnxruntime_USE_OPENMP=ON `
+        -D onnxruntime_USE_PREINSTALLED_EIGEN=ON
 
     mingw32-make -j 4
     mingw32-make install

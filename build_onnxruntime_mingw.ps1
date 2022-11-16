f86f81af5bacbac0c1b7c7d193866fd0c8b09679
jiamian2009 <48003443+jiamian2009@users.noreply.github.com>
2022-11-16 09:39:12 +0800

    Update build_onnxruntime_mingw.ps1

--- build_onnxruntime_mingw.ps1
+++ build_onnxruntime_mingw.ps1
@@ -16,7 +16,7 @@ function BuildForWindows($platform, $build_type) {
         -D CMAKE_BUILD_TYPE=${build_type} `
         -D CMAKE_INSTALL_PREFIX=mingw64 `
         -D Protobuf_USE_STATIC_LIBS=ON `
-        -D eigen_SOURCE_PATH=/mingw64/include/eigen3 `
+        -D eigen_SOURCE_PATH=mingw64/include/eigen3 `
         -D onnxruntime_BUILD_BENCHMARKS=OFF `
         -D onnxruntime_BUILD_SHARED_LIB=ON `
         -D onnxruntime_BUILD_UNIT_TESTS=OFF `
@@ -34,7 +34,7 @@ function BuildForWindows($platform, $build_type) {
         -D onnxruntime_USE_FULL_PROTOBUF=ON `
         -D onnxruntime_USE_MPI=ON `
         -D onnxruntime_USE_OPENMP=ON `
-        -D onnxruntime_USE_PREINSTALLED_EIGEN=ON ..
+        -D onnxruntime_USE_PREINSTALLED_EIGEN=ON ../onnxruntime
 
     mingw32-make -j 4
     mingw32-make install
